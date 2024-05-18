3c3bf05def00093ce5a9df4734a71bb85137b973
Aryan Pandey <aryan.lko04@gmail.com>
2024-05-18 17:11:46 +0530

    xc

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Navigation from '@/components/Navigation'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Webchain dev',
+  title: 'Aryan Pandey',
   description: 'This is my portfolio',
 }
 
